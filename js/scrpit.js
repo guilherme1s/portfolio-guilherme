afa374c01c6fc7e4c2bcb6ffd934b28c4b81238c
guilherme1s <2000guilherme.silva@gmail.com>
2024-03-21 11:50:23 -0300

    feat: create-modal

--- js/scrpit.js
+++ js/scrpit.js
@@ -1,3 +1,18 @@
+const getModalHTML = document.querySelector(".skillHTML");
+const getModalCSS = document.querySelector(".skillCSS");
+const getModalJS = document.querySelector(".skillJS"); 
+const getModalReact = document.querySelector(".skillReact"); 
+const getModalC = document.querySelector(".skillsC"); 
+const getModalPy = document.querySelector(".skillPy"); 
+const closeModals = document.querySelectorAll(".closeModalButton"); 
+
+const modalHTML = document.querySelector(".modalHTML");
+const modalCSS = document.querySelector(".modalCSS");
+const modalJS = document.querySelector(".modalJS");
+const modalReact = document.querySelector(".modalReact");
+const modalC = document.querySelector(".modalC");
+const modalPy = document.querySelector(".modalPy");
+
 function openMenu() {
     let menuOptions = document.querySelector('.menuNavOptions');
     menuOptions.classList.toggle('show');
@@ -18,3 +33,36 @@ function scrollToSection(sectionId) {
         });
     }
 }
+
+getModalHTML.onclick = function() {
+    modalHTML.showModal();  
+}
+
+getModalCSS.onclick = function() {
+    modalCSS.showModal();
+}
+
+getModalJS.onclick = function() {
+    modalJS.showModal();
+}
+
+getModalReact.onclick = function() {
+    modalReact.showModal();
+}
+
+getModalC.onclick = function() {
+    modalC.showModal();
+}
+
+getModalPy.onclick = function() {
+    modalPy.showModal();
+}
+
+closeModals.forEach(function(button) {
+    button.onclick = function() {
+        const openModals = document.querySelectorAll('dialog[open]');
+        openModals.forEach(function(modal) {
+            modal.close();
+        });
+    };
+});
